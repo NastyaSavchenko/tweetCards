720573834c335d83cc5665a9fadc5c47fb61a884
Анастасія Савченко <savyle4ka13@gmail.com>
2023-06-17 23:11:31 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { App } from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/tweets">
+    <BrowserRouter basename="/tweetCards">
       {/* <BrowserRouter> */}
       <App />
     </BrowserRouter>
